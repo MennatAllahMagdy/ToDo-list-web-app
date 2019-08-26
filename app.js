c7fdfc46125bca66361d75e7d2854f980ab6339e
MennatAllahMagdy <mennamagdykh@gmail.com>
2019-08-27 01:20:00 +0200

    Changed port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var PORT= process.env.PORT||3000;
 var mycontrollers= require('./controllers/todocontroller');
 var app= express();
 //css template
@@ -7,5 +7,5 @@ app.set('view engine','ejs');
 //static files
 app.use(express.static('./public'));
 mycontrollers(app);
-app.listen(3000);
+app.listen(PORT);
 console.log('listen to 3000');
